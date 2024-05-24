644f6b34d250dc8ef181f7ebac3d5536a5c8e9e6
Vadym Foteniuk <vadumfotenuk@gmail.com>
2024-05-25 01:00:35 +0300

    Add cart logic

---
+++ js/beats.js
@@ -0,0 +1,44 @@
+const beats = [
+    {
+        id: 0,
+        img: "img/product/p1.png",
+        name: "Item Title 1",
+        subtitle: "1Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, enim? Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, nisi. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, culpa."
+    },
+    {
+        id: 1,
+        img: "img/product/p2.png",
+        name: "Item Title 2",
+        subtitle: "2Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, enim? Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, nisi. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, culpa."
+    },
+    {
+        id: 2,
+        img: "img/product/p3.png",
+        name: "Item Title 3",
+        subtitle: "3Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, enim? Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, nisi. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, culpa."
+    },
+    {
+        id: 3,
+        img: "img/product/p2.png",
+        name: "Item Title 4",
+        subtitle: "4Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, enim? Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, nisi. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, culpa."
+    },
+    {
+        id: 4,
+        img: "img/product/p3.png",
+        name: "Item Title 5",
+        subtitle: "5Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, enim? Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, nisi. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, culpa."
+    },
+    {
+        id: 5,
+        img: "img/product/p1.png",
+        name: "Item Title 6",
+        subtitle: "6Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, enim? Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, nisi. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, culpa."
+    },
+    {
+        id: 6,
+        img: "img/product/p2.png",
+        name: "Item Title 7",
+        subtitle: "7Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, enim? Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, nisi. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, culpa."
+    },
+]
--- js/script.js
+++ js/script.js
@@ -1,8 +1,72 @@
 let cardOverflow = document.querySelector(".card__overflow")
 let cartMenu = document.querySelector(".cart__menu")
 let html = document.querySelector("html")
+let mainContainer = document.querySelector(".main__container")
 
 cartMenu.addEventListener("click", ()=>{
     cardOverflow.classList.toggle("display")
     html.classList.toggle("overflow")
 })
+beats.forEach((elem)=> {
+    mainContainer.innerHTML += `
+    <div class="main__item">
+        <center>
+            <img class="main__img" src=${elem.img} alt="shop product">
+        </center>
+        <h2 class="main__title">${elem.name}</h2>
+        <p class="main__info">${elem.subtitle}</p>
+        <div class="main__interaction">
+            <i class="fa-solid fa-heart"></i>
+            <i onClick="addToCart(${elem.id})" class="fa-solid fa-cart-shopping"></i>
+        </div>
+    </div>
+    `
+})
+
+let cardMain = document.querySelector(".card__main")
+
+let shopCart = []
+function addToCart(id) {
+    let findElem = shopCart.find((elem)=> elem.id == id)
+    if(findElem) {
+        findElem.count++
+    } else {
+        let element = beats.find((el)=> el.id == id)
+        shopCart.push({
+            ...element,
+            count: 1
+        })
+    }
+    console.log(shopCart);
+    updateCart()
+}
+function updateCart() {
+    cardMain.innerHTML = ''
+    shopCart.forEach((element)=> {
+        cardMain.innerHTML += `
+        <div class="card__main-item">
+            <button class="card__close">&#10006;</button>
+            <div class="card__main-header">
+                <img src=${element.img} alt="cart image">
+                <h3 class="card__main-title">${element.name}</h3>
+            </div>
+            <p class="card__main-subtitle">${element.subtitle}</p>
+            <div class="card__amount">
+                <button onClick="changeCount('-', ${element.id})">-</button>
+                <p class="card__count">${element.count}</p>
+                <button onClick="changeCount('+', ${element.id})">+</button>
+            </div>
+        </div>
+        `
+    })
+}
+function changeCount(action, id) {
+    let item = shopCart.find((elem)=> elem.id == id)
+    if(action == "-" && item.count > 1) {
+        item.count--
+    } else if(action == "+") {
+        item.count++
+    }
+    updateCart()
+}
+
